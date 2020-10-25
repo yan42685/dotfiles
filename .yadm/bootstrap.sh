f8b0c3ffbcc37a5bf2fba891fbcd57b3895d763a
alex <419725794@qq.com>
2020-10-25 12:09:54 +0800

    把&&改成多行语句

--- .yadm/bootstrap.sh
+++ .yadm/bootstrap.sh
@@ -9,15 +9,16 @@ system_type=$(uname -s)
 setup_ubuntu_environment() {
 
     echo "==================== updating packages..."
-    sudo apt update && sudo apt -y upgrade
+    sudo apt update
+    sudo apt -y upgrade
     echo "==================== Installing required packages..."
     sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
 
     echo "==================== setuping basic packages..."
     sudo apt install -y nodejs npm python3-pip zsh lua5.3
-    # 更新pip3版本
-    wget https://bootstrap.pypa.io/get-pip.py
-    sudo python3 get-pip.py
+    # 更新pip和pip3版本
+    pip install --upgrade pip
+    pip3 install --upgrade pip
     # 更新node版本
     sudo npm cache clean -f
     sudo npm install -g n
@@ -36,12 +37,17 @@ setup_ubuntu_environment() {
     echo "==================== installing applications"
     sudo apt install -y trash-cli nnn gdb-dashboard
     # 安装zeal
-    sudo add-apt-repository ppa:zeal-developers/ppa -y && sudo apt-get update && sudo apt-get install -y zeal
+    sudo add-apt-repository ppa:zeal-developers/ppa -y
+    sudo apt-get update
+    sudo apt-get install -y zeal
 
     echo "==================== installing linter and checker"
-    npm install -g eslint && npm install -g prettier
-    pip3 install pylint && pip3 install autopep8
-    apt install cppcheck -y && npm install -g clang-format
+    npm install -g eslint
+    npm install -g prettier
+    pip3 install pylint
+    pip3 install autopep8
+    apt install cppcheck -y
+    npm install -g clang-format
 
     echo "==================== installing neovim-remote"
     pip3 install neovim-remote
@@ -53,14 +59,17 @@ setup_ubuntu_environment() {
         wget https://ftp.gnu.org/pub/gnu/global/global-6.6.tar.gz
         tar xvf global-6.6.tar.gz
         cd global-6.6/
-        ./configure && make && sudo make install
+        ./configure
+        make
+        sudo make install
         cd ~ && rm -r global-6.6/ && rm global-6.6.tar.gz
     fi
 
 
     if ! command -v rg >/dev/null 2>&1; then
         echo "==================== installing riggrep"
-        curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb && sudo dpkg -i ripgrep_11.0.2_amd64.deb
+        curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
+        sudo dpkg -i ripgrep_11.0.2_amd64.deb
         rm ripgrep_11.0.2_amd64.deb
     fi
 
@@ -91,7 +100,8 @@ setup_ubuntu_environment() {
         sudo add-apt-repository ppa:mmstick76/alacritty -y
         # 添加缺失的public key
         sudo apt update 2>&1 1>/dev/null | sed -ne 's/.*NO_PUBKEY //p' | while read key; do if ! [[ ${keys[*]} =~ "$key" ]]; then sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net:80 --recv-keys "$key"; keys+="$key"; fi; done
-        sudo apt-get update && sudo apt install -y alacritty
+        sudo apt-get update
+        sudo apt install -y alacritty
     fi
 
     echo "==================== 更换默认bash为zsh..."
