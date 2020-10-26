fb45538ae4aed59fb8b48470a67242e1b1318ffe
alex <419725794@qq.com>
2020-10-26 10:12:29 +0800

    refactor

--- .my-scripts/check_commands.sh
+++ .my-scripts/check_commands.sh
@@ -1,5 +1,8 @@
 missing_commands_count=0
+missing_directories_count=0
 missing_files_count=0
+is_missing=flase
+
 
 commands=(
 node npm pip3 zsh lua5.3 pyenv
@@ -7,8 +10,10 @@ nvim ccls ctags trash nnn zeal
 gtags rg tmux fzf alacritty
 )
 
+directories=(
+)
+
 files=(
-~/.tmux/plugins/tpm
 )
 
 for cmd in ${commands[@]};
@@ -16,6 +21,14 @@ do
     if ! command -v $cmd >/dev/null 2>&1; then
         echo "command $cmd not exists!"
         let missing_commands_count++
+    fi
+done
+
+for directory in ${directories[@]};
+do
+    if ! -d $directory>/dev/null 2>&1; then
+        echo "directory $directory not exists!"
+        let missing_directories_count++
     fi
 done
 
@@ -27,17 +40,21 @@ do
     fi
 done
 
-is_missing=flase
-
 if [[ $missing_commands_count != 0 ]]; then
     is_missing=true
     echo "Missing ${missing_commands_count} commands!"
+fi
+
+if [[ $missing_directories_count != 0 ]]; then
+    is_missing=true
+    echo "Missing ${missing_directories_count} directories!"
 fi
 
 if [[ $missing_files_count != 0 ]]; then
     is_missing=true
     echo "Missing ${missing_files_count} files!"
 fi
+
 
 if [[ $is_missing ]]; then
     # 用于后续$?处理
