2c5b87e09c9e350201a8c8a0c6be690f02c9feed
alexyan <41834091+yan42685@users.noreply.github.com>
2020-11-03 05:56:50 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,25 +4,26 @@ using namespace std;
 int main(int argc, const char *argv[]) { return 0; }
 
 void test1() {
+    cout << "remote add" << endl;
+    cout << "remote add" << endl;
+    cout << "remote add" << endl;
+    cout << "remote add" << endl;
+    cout << "remote add" << endl;
+    cout << "remote add" << endl;
     cout << "hello" << endl;
     cout << "hello" << endl;
     cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
+   
 
     cout << "hello" << endl;
     cout << "hello" << endl;
     cout << "hello" << endl;
+    cout << "remote change" << endl;
+    cout << "remote change" << endl;
+    cout << "remote change" << endl;
     cout << "hello" << endl;
     cout << "hello" << endl;
     cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
+   
     return;
 }
