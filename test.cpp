db60b411c89cd9b197e2c4bc49896d2eb36f812f
alex <419725794@qq.com>
2020-11-03 04:56:58 -0800

    test mergetool

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int main(int argc, const char *argv[]) { return 0; }
+
+void test1() {
+    cout << "hello" << endl;
+    return;
+}
