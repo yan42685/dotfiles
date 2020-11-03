c273a32104b679b5bcb270c99887e365b75f35a9
alex <419725794@qq.com>
2020-11-03 04:59:08 -0800

    test mergetool

--- test.cpp
+++ test.cpp
@@ -4,6 +4,21 @@ using namespace std;
 int main(int argc, const char *argv[]) { return 0; }
 
 void test1() {
-    cout << "hello" << endl;
+    cout << "gretting acdef" << endl;
+    cout << "gretting acdef" << endl;
+    cout << "gretting acdef" << endl;
+    cout << "gretting acdef" << endl;
+    cout << "gretting acdef" << endl;
+    cout << "gretting acdef" << endl;
+
+    cout << "gretting acdef" << endl;
+    cout << "gretting acdef" << endl;
+    cout << "gretting acdef" << endl;
+    cout << "gretting acdef" << endl;
+
+    cout << "gretting acdef" << endl;
+    cout << "gretting acdef" << endl;
+    cout << "gretting acdef" << endl;
+    cout << "gretting acdef" << endl;
     return;
 }
