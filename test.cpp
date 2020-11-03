db018d8c3dbe57251e437f273e26a2930937b695
alex <419725794@qq.com>
2020-11-03 05:57:00 -0800

    test mergetool

--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <cstdio>
 #include <iostream>
 using namespace std;
 int main(int argc, const char *argv[]) { return 0; }
@@ -7,9 +8,9 @@ void test1() {
     cout << "hello" << endl;
     cout << "hello" << endl;
     cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
+    cout << "local change" << endl;
+    cout << "local change" << endl;
+    cout << "local change" << endl;
     cout << "hello" << endl;
     cout << "hello" << endl;
     cout << "hello" << endl;
@@ -19,10 +20,13 @@ void test1() {
     cout << "hello" << endl;
     cout << "hello" << endl;
     cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
+
+    printf("local local local local local");
+    printf("local local local local local");
+    printf("local local local local local");
+    printf("local local local local local");
+    printf("local local local local local");
+    printf("local local local local local");
+    printf("local local local local local");
     return;
 }
